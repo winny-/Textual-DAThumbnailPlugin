a862b9f1d41514869c724b580942e4499abad9bf
Winston Weinert <winston@ml1.net>
2014-11-04 16:02:23 -0600

    Attempt to fix Textual crashes

--- DAThumbnailPlugin.swift
+++ DAThumbnailPlugin.swift
@@ -1,26 +1,18 @@
-
-/* We create a class that inherits from NSObject so we need Foundation. */
 import Foundation
 
-/* As Textual creates a new instance of our primary class when the plugin
-loads, it must inherit NSObject to allow proper initialization. */
-/* THOPluginProtocol is the protocol available for plugin specific callbacks.
-It is appended to our new class object to inform swift that we conform to it.
-However, all methods in this class are optional. The plugin does not have to
-inherit anyone of them and can instead manipulate using any calls within its
-public header files available at /Applications/Textual.app/Contents/Headers/ */
 class WNY_DAThumbnailPlugin: NSObject, THOPluginProtocol
 {
     let DeviantArtRegex = "http://([a-z0-9_-]+\\.deviantart\\.com/art/[a-z0-9_-]+|" +
         "fav\\.me/[a-z0-9_-]+|" +
         "sta\\.sh/[a-z0-9_-]+|" +
-    "|[a-z0-9_-]+\\.deviantart\\.com/[a-z0-9_]+#/d[a-z0-9_-]+)"
+        "|[a-z0-9_-]+\\.deviantart\\.com/[a-z0-9_]+#/d[a-z0-9_-]+)"
     let DeviantArtOEmbed = "http://backend.deviantart.com/oembed?url=%@"
+    var regex: NSRegularExpression
     
     func isDeviantArtURL(url: String) -> Bool {
-        var error: NSError?
-        let r = NSRegularExpression(pattern: DeviantArtRegex, options: .CaseInsensitive, error: &error)!
-        return r.numberOfMatchesInString(url, options: nil, range: NSMakeRange(0, countElements(url))) > 0
+        let rangeOfUrl = NSMakeRange(0, countElements(url))
+        let matches = regex.numberOfMatchesInString(url, options: nil, range: rangeOfUrl)
+        return matches > 0
     }
     
     func thumbForDeviantArtURL(url: String) -> String? {
@@ -31,21 +23,25 @@ class WNY_DAThumbnailPlugin: NSObject, THOPluginProtocol
         let request = NSURLRequest(URL: requestURL)
         var reqError: NSError?
         let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: &reqError)
+        if reqError != nil || data == nil { return nil }
         var jsonError: NSError?
         let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary
-        if jsonResult != nil && jsonResult.objectForKey("thumbnail_url") != nil {
+        if jsonError == nil && jsonResult != nil && jsonResult.objectForKey("thumbnail_url") != nil {
             return jsonResult["thumbnail_url"]! as? String
         } else {
             return nil
         }
     }
     
+    override init() {
+        self.regex = NSRegularExpression(pattern: DeviantArtRegex, options: .CaseInsensitive, error: nil)!
+        super.init()
+    }
+    
     func processInlineMediaContentURL(resource: String!) -> String! {
         if isDeviantArtURL(resource) {
             let thumb = thumbForDeviantArtURL(resource)
-            if thumb != nil {
-                return thumb!
-            }
+            if thumb != nil { return thumb! }
         }
         return nil
     }
