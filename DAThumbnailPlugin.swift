99367ac32fbe6d115303cc2a7ffb3fc8c9130ca8
Winston Weinert <winston@ml1.net>
2014-11-05 13:48:53 -0600

    Swift is absolute shit.

--- DAThumbnailPlugin.swift
+++ DAThumbnailPlugin.swift
@@ -9,39 +9,50 @@ class WNY_DAThumbnailPlugin: NSObject, THOPluginProtocol
     let DeviantArtOEmbed = "https://backend.deviantart.com/oembed?url=%@"
     var regex: NSRegularExpression
     
-    func isDeviantArtURL(url: String) -> Bool {
-        let rangeOfUrl = NSMakeRange(0, countElements(url))
-        let matches = regex.numberOfMatchesInString(url, options: nil, range: rangeOfUrl)
-        return matches > 0
-    }
-    
-    func thumbForDeviantArtURL(url: String) -> String? {
-        let escaped = url.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet())!
-        
-        let requestString = String(format: DeviantArtOEmbed, escaped)
-        let requestURL = NSURL(string: requestString)!
-        let request = NSURLRequest(URL: requestURL)
-        var reqError: NSError?
-        let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: &reqError)
-        if reqError != nil || data == nil { return nil }
-        var jsonError: NSError?
-        let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary
-        if jsonError == nil && jsonResult != nil && jsonResult.objectForKey("thumbnail_url") != nil {
-            return jsonResult["thumbnail_url"]! as? String
-        } else {
-            return nil
-        }
-    }
-    
     override init() {
         self.regex = NSRegularExpression(pattern: DeviantArtRegex, options: .CaseInsensitive, error: nil)!
         super.init()
     }
     
+    func isDeviantArtURL(url: String?) -> Bool {
+        if let unwrapped = url {
+            let rangeOfUrl = NSMakeRange(0, countElements(unwrapped))
+            let matches = regex.numberOfMatchesInString(unwrapped, options: nil, range: rangeOfUrl)
+            return matches > 0
+        }
+        return false
+    }
+
+    func thumbForDeviantArtURL(url: String) -> String? {
+        if let req = buildDAEmbedRequest(url) {
+            if let data = NSURLConnection.sendSynchronousRequest(req, returningResponse: nil, error: nil) {
+                if let res = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary {
+                    return res.objectForKey("thumbnail_url") as? String
+                }
+            }
+        }
+        return nil
+    }
+    
+    func buildDAEmbedRequest(url: String?) -> NSURLRequest? {
+        if let unwrapped = url {
+            if let escaped = unwrapped.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet()) {
+                let urlString = String(format: DeviantArtOEmbed, escaped)
+                if let u = NSURL(string: urlString) {
+                    return NSURLRequest(URL: u)
+                }
+            }
+        }
+        return nil
+    }
+    
     func processInlineMediaContentURL(resource: String!) -> String! {
-        if isDeviantArtURL(resource) {
-            let thumb = thumbForDeviantArtURL(resource)
-            if thumb != nil { return thumb! }
+        if let r = resource {
+            if isDeviantArtURL(r) {
+                if let thumb = thumbForDeviantArtURL(r) {
+                    return thumb
+                }
+            }
         }
         return nil
     }
